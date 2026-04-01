307ac8e2f5a704b80b1147b89b8dba786e62ecf6
Jaspreet Singh <jaspreet2005g@gmail.com>
2026-04-01 15:27:34 +0530

    Fixed PORT for Render deployment

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ require("dotenv").config();
 const app = require('./src/app');
 const connectDB = require('./src/services/db');
 
+if (!process.env.MONGO_URI) {
+  console.error("MONGO_URI is missing!");
+  process.exit(1);
+}
 connectDB(); //connecting to database
 
-app.listen(3000 , ()=>{
+app.listen(10000 , ()=>{
     console.log("App listening on port 3000");
 })
